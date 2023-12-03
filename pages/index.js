4cd4338b1628731a6d345bbe03059317d94dff0b
Radhika Gupte <gupter@oregonstate.edu>
2023-12-02 16:45:57 -0800

    added third party Javascript

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Check out <a href="/posts/first-post">this page!</a>
+          Check out <Link href="/posts/first-post">this page!</Link>
         </h1>
 
         <p className={styles.description}>
